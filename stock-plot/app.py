8e597a73116aec1fe560dfc521256135d4eeda53
gokulac <kumarangokul@gmail.com>
2021-06-17 23:53:36 +0530

    removed URI

--- stock-plot/app.py
+++ stock-plot/app.py
@@ -1,22 +1,35 @@
 import matplotlib.pyplot as plt
 import csv
+from flask import Flask
+
+app = Flask(__name__)
 
 x = []
 y = []
 
-with open('returns.csv','r') as csvfile:
-	lines = csv.reader(csvfile, delimiter=',')
-	for row in lines:
-		x.append(row[0])
-		y.append(int(row[1]))
+@app.route("/")
+def plot():
 
-plt.plot(x, y, color = 'g', linestyle = 'dashed',
-		marker = 'o',label = "Weather Data")
 
-plt.xticks(rotation = 25)
-plt.xlabel('Dates')
-plt.ylabel('Returns')
-plt.title('SWAT Fin Exp', fontsize = 20)
-plt.grid()
-plt.legend()
-plt.show()
+    with open('returns.csv','r') as csvfile:
+        lines = csv.reader(csvfile, delimiter=',')
+        for row in lines:
+            x.append(row[0])
+            y.append(int(row[1]))
+
+    plt.plot(x, y, color = 'g', linestyle = 'dashed',
+            marker = 'o',label = "Stock Returns")
+
+    plt.xticks(rotation = 25)
+    plt.xlabel('Dates')
+    plt.ylabel('Returns')
+    plt.title('SWAT Fin Exp', fontsize = 20)
+    # plt.grid()
+    # plt.legend()
+    # final_output = plt.show()
+
+    return plt.show()
+
+
+if __name__ == "__main__":
+    app.run()
